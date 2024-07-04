9fdae6996dc79b3a903f26cc3d1ac8c280305f95
saiteja cherviralla <saiteja.ch.st1@gmail.com>
2024-07-04 08:25:02 +0000

    new changes added

---
+++ UI/plants/webapp/controller/View1.controller.js
@@ -0,0 +1,12 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+],
+function (Controller) {
+    "use strict";
+
+    return Controller.extend("plants.controller.View1", {
+        onInit: function () {
+
+        }
+    });
+});
---
+++ UI/plants/webapp/controller/View2.controller.js
@@ -0,0 +1,12 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+],
+function (Controller) {
+    "use strict";
+
+    return Controller.extend("plants.controller.View2", {
+        onInit: function () {
+
+        }
+    });
+});
