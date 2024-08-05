6935370968f95611f88e6f7a2d4cd0e9b0bbce5f
saiteja cherviralla <saiteja.ch.st1@gmail.com>
2024-08-05 09:21:18 +0000

    new changes added

--- UI/product_master/webapp/controller/View1.controller.js
+++ UI/product_master/webapp/controller/View1.controller.js
@@ -8,6 +8,11 @@ function (Controller,fioriLibary) {
     return Controller.extend("productmaster.controller.View1", {
         onInit: function () {
             that = this;
+            if (!that.create) {
+
+                that.create = sap.ui.xmlfragment("productmaster.view.create_product", that);
+            }
+           
         },
         handle_press:function()
         {
@@ -22,6 +27,39 @@ function (Controller,fioriLibary) {
              var oSecondController = sap.ui.controller("productmaster.controller.View2"); 
              oSecondController.Bind_Object();
              oView.setLayout(fioriLibary.LayoutType.TwoColumnsMidExpanded);
+        },
+        create_:function()
+        {
+            sap.ui.getCore().byId("pname").setValue("")
+            sap.ui.getCore().byId("pdescr").setValue("")
+            sap.ui.getCore().byId("quantity").setValue("")
+            sap.ui.getCore().byId("avatar").getValue("")
+            that.create.open()
+        },
+        close_:function()
+        {
+            that.create.close()
+        },
+        submit_create:function()
+        {
+            let obj = {
+                PRODUCT_NAME:sap.ui.getCore().byId("pname").getValue(),
+                PRODUCT_DESC:sap.ui.getCore().byId("pdescr").getValue(),
+                QUAN_AVAIL:sap.ui.getCore().byId("quantity").getValue(),
+                AVATAR:sap.ui.getCore().byId("avatar").getValue()
+            }
+
+            that.getOwnerComponent().getModel().create('/MaterialManagement',obj,{
+                success:function(response)
+                {
+                    console.log(response)
+                    that.create.close()
+                },
+                error:function(error)
+                {
+                    console.log(error)
+                }
+            })
         }
     });
 });
