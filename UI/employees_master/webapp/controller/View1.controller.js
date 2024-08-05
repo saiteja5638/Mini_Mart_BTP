72cc92273da41bdbabf6de33a74631f9f503fdab
saiteja cherviralla <saiteja.ch.st1@gmail.com>
2024-08-05 11:32:37 +0000

    new changes added

--- UI/employees_master/webapp/controller/View1.controller.js
+++ UI/employees_master/webapp/controller/View1.controller.js
@@ -8,6 +8,12 @@ function (Controller,fioriLibary) {
     return Controller.extend("employeesmaster.controller.View1", {
         onInit: function () {
             that = this;
+
+            if (!that.create) {
+
+                that.create = sap.ui.xmlfragment("employeesmaster.view.create_emp", that);
+            }
+          
         },
         on_trigger:function()
         {
@@ -18,6 +24,44 @@ function (Controller,fioriLibary) {
             var oSecondController = sap.ui.controller("employeesmaster.controller.View2"); // Replace with the actual ID of the second controller
             oSecondController.Bind_Object();
             oView.setLayout(fioriLibary.LayoutType.TwoColumnsMidExpanded);
+        },
+        create_:function()
+        {
+            sap.ui.getCore().byId("pname").setValue("")
+            sap.ui.getCore().byId("_IDGenInput1").setValue("")
+            sap.ui.getCore().byId("_IDGenInput2").setValue("")
+            sap.ui.getCore().byId("_IDGenInput3").setValue("")
+            sap.ui.getCore().byId("_IDGenInput4").setValue("")
+            sap.ui.getCore().byId("_IDGenInput5").setValue("")
+            that.create.open()
+        },
+        close_:function()
+        {
+            that.create.close()
+        },
+        submit_create:function()
+        {
+            let obj = {
+                PLANT_ID: "EMP"+ Math.floor(Math.random()*85222),
+                PLANT_NAME:sap.ui.getCore().byId("pname").getValue(),
+                PLANT_LOC:sap.ui.getCore().byId("lname").getValue(),
+                PLANT_CONT:sap.ui.getCore().byId("pcontact").getValue(),
+                PLANT_EMAIL:sap.ui.getCore().byId("pemail").getValue(),
+                PLANT_HEAD:sap.ui.getCore().byId("phead").getValue(),
+                PLANT_AVATAR:sap.ui.getCore().byId("avatar").getValue()
+            }
+
+            that.getOwnerComponent().getModel().create('/Employees',obj,{
+                success:function(response)
+                {
+                    console.log(response)
+                    that.create.close()
+                },
+                error:function(error)
+                {
+                    console.log(error)
+                }
+            })
         }
     });
 });
